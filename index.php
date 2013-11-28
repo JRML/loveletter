0ff842570a61896cb6d017411e3270c6f44ca592
JRML <writingcoastlines@gmail.com>
2013-11-28 13:20:44 +0000

    Writing Coastlines Letters
    Writing Coastlines Letters is based on the source code of Matt Sephton's
    php iteration of Christopher Strachey's Love Letter Generator, written
    on the Manchester University Computer in 1952.
    
    Strachey used a thesaurus to select variables for his Love Letter
    Generator, making a mockery of 'mechanical' modes of expressing love.
    Writing Coastlines Letters replaces the variables in Christopher
    Strachey's Love Letter Generator with variables culled from my PhD
    research, making a mockery of 'mechanical' modes of academic writing.
    The title Writing Coastlines Letters refers to the title of my thesis is
    "Writing Coastlines: Locating Narrative Resonance in Transatlantic
    Communications Networks".

--- index.php
+++ index.php
@@ -3,98 +3,99 @@
 
 <html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="reset.css" type="text/css" media="screen" charset="utf-8">
-	<style type="text/css" media="screen">
-		body {
-			margin: 1em;
-		}
-		
-		a {
-			color: #E50;
-		}
-		
-		em {
-			display: block;
-			width: 35em;
-			font-style: normal;
-			color: #000;
-		}
-		
-		pre {
-			white-space: pre-wrap;
-			text-transform: uppercase;
-			line-height: 1.7em;
-			color: #999;
-		}
-		
-		ul li {
-			float: left;
-			margin-right: 2em;
-		}
-	</style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" href="reset.css" type="text/css" media="screen" charset="utf-8">
+<style type="text/css" media="screen">
+body {
+margin: 1em;
+}
+
+a {
+color: #E50;
+}
+
+em {
+display: block;
+width: 45em;
+font-style: normal;
+color: #000;
+}
+
+pre {
+white-space: pre-wrap;
+text-transform: uppercase;
+line-height: 1.7em;
+color: #999;
+}
+
+ul li {
+float: left;
+margin-right: 2em;
+}
+</style>
 </head>
 <body>
 <pre>
-<strong>Christopher Strachey "Loveletters" (1952)</strong>
+<strong>J. R. Carpenter "Writing Coastlines Letters" (2013)</strong>
+<strong>After Christopher Strachey "Loveletters" (1952)</strong>
 
 <?php
 
-$sals1 = array("Beloved", "Darling", "Dear", "Dearest", "Fanciful", "Honey");
+$sals1 = array("North Atlantic", "Writing and Erasing", "Reading and Writing", "Coming and Going", "Performing", "Shifting", "Transmitting and Receiving", "Calling and Responding", "Ever Questioning");
 
-$sals2 = array("Chickpea", "Dear", "Duck", "Jewel", "Love", "Moppet", "Sweetheart");
+$sals2 = array("Briny Beaches", "Coastlines", "Communications Networks", "Legible Lines", "Locations in Memory", "Narratives", "Narrative Dialogues", "Non-Places", "Liminal Spaces", "In-Between Spaces", "Passages", "Points of Departure", "Lines of Flight", "Topics", "Topographies", "Terrains");
 
-$adjs = array("affectionate", "amorous", "anxious", "avid", "beautiful", "breathless", "burning", "covetous", "craving", "curious", "eager", "fervent", "fondest", "loveable", "lovesick", "loving", "passionate", "precious", "seductive", "sweet", "sympathetic", "tender", "unsatisfied", "winning", "wistful");
+$adjs = array("algorithmic", "archival", "cartographic", "combinatory", "conceptual", "computational", "computer-generated", "critical", "dialogic", "digital", "digital literary", "electronic", "electromagnetic", "embodied", "haunted", "hybrid", "interdisciplinary", "iterative", "literary", "locative", "Marxist", "machinic", "mediated", "media archaeological", "multi-modal", "narrative", "performative", "performance writing", "pragmatic", "situationist", "synthetic", "shifting", "uncanny", "unhomed", "variable", "web-based");
 
-$nouns = array("adoration", "affection", "ambition", "appetite", "ardour", "being", "burning", "charm", "craving", "desire", "devotion", "eagerness", "enchantment", "enthusiasm", "fancy", "fellow feeling", "fervour", "fondness", "heart", "hunger", "infatuation", "little liking", "longing", "love", "lust", "passion", "rapture", "sympathy", "thirst", "wish", "yearning");
+$nouns = array("aim", "approach", "argument", "assemblage", "bibliography", "citation", "conclusion", "context", "contribution", "corpus", "chapter", "chapter outline", "critique", "dialectic", "draft", "essay", "experiment", "interest", "intervention", "iteration", "letter", "line of inquiry", "method", "objective", "paragraphy", "practice", "research", "reading", "conference paper", "mediation", "performance", "publication", "question", "quotation", "speech act", "test", "text", "transmission", "translation", "transmediation", "transmutation", "study", "subject", "thesis", "title", "utterance", "version", "work", "writing");
 
-$advs = array("affectionately", "ardently", "anxiously", "beautifully", "burningly", "covetously", "curiously", "eagerly", "fervently", "fondly", "impatiently", "keenly", "lovingly", "passionately", "seductively", "tenderly", "wistfully");
+$advs = array("algorithmically", "analytically", "bodily", "carefully", "computationally", "creatively", "critically", "curiously", "digitally", "diligently", "eagerly", "electronically", "electromagnetically", "impatiently", "keenly", "methodically", "methodologically", "necessarily", "originally", "performatively", "physically", "playfully", "seriously", "studiously", "stylistically", "systematically", "sceptically", "scholarly", "systematically", "technically", "textually");
 
-$verbs = array("adores", "attracts", "clings to", "holds dear", "hopes for", "hungers for", "likes", "longs for", "loves", "lusts after", "pants for", "pines for", "sighs for", "tempts", "thirsts for", "treasures", "yearns for", "woos");
+$verbs = array("argues", "charts", "communicates", "confuses", "confounds", "connects", "considers", "demonstrates", "describes", "engages with", "erases", "examines", "explains", "explores", "generates", "haunts", "identifies", "illustrates", "listens for", "locates", "makes public", "makes clear", "maps", "networks", "outlines", "performs", "programmes", "publishes", "reads", "re-reads", "reiterates", "researches", "receives", "resonates with", "transmits", "speaks", "states", "studies", "suggests", "thwarts", "walks along", "writes", "re-writes");
 
 define(SHORT, 1);
 define(LONG, 2);
 $last = NULL;
 
-$ll = sprintf("%s %s,\n     ", rel($sals1), rel($sals2));
+$ll = sprintf("%s %s,\n ", rel($sals1), rel($sals2));
 for($i=0; $i<5; $i++) {
-	if (mt_rand(0,9) < 5) {
-		//LONG
-		$optadj1 = (mt_rand(0,9) < 5) ? '' : rel($adjs);
-		$noun1 = rel($nouns);
-		$optadv = (mt_rand(0,9) < 5) ? '' : rel($advs);
-		$verb = rel($verbs);
-		$optadj2 = (mt_rand(0,9) < 5) ? '' : rel($adjs);
-		$noun2 = rel($nouns);
+if (mt_rand(0,9) < 5) {
+//LONG
+$optadj1 = (mt_rand(0,9) < 5) ? '' : rel($adjs);
+$noun1 = rel($nouns);
+$optadv = (mt_rand(0,9) < 5) ? '' : rel($advs);
+$verb = rel($verbs);
+$optadj2 = (mt_rand(0,9) < 5) ? '' : rel($adjs);
+$noun2 = rel($nouns);
 
-		if ($last != NULL || $last == LONG) {
-			$concat = ". ";
-		}
+if ($last != NULL || $last == LONG) {
+$concat = ".";
+}
 
-		$ll .= sprintf("%s My %s %s %s %s your %s %s", $concat, $optadj1, $noun1, $optadv, $verb, $optadj2, $noun2);
-		$last = LONG;
+$ll .= sprintf("%s My %s %s %s %s your %s %s", $concat, $optadj1, $noun1, $optadv, $verb, $optadj2, $noun2);
+$last = LONG;
 
-	} else {
-		//SHORT
-		$adj = rel($adjs);
-		$noun = rel($nouns);
-		if ($last == SHORT) {
-			$concat = ", ";
-		} elseif ($last == LONG) {
-			$concat = ". You are";
-		} else {
-			$concat = "You are ";
-		}
+} else {
+//SHORT
+$adj = rel($adjs);
+$noun = rel($nouns);
+if ($last == SHORT) {
+$concat = ",";
+} elseif ($last == LONG) {
+$concat = ". You are";
+} else {
+$concat = "You are";
+}
 
-		$ll .= sprintf("%s my %s %s", $concat, $adj, $noun);
-		$last = SHORT;
-	}
+$ll .= sprintf("%s my %s %s", $concat, $adj, $noun);
+$last = SHORT;
+}
 }
 $adv = rel($advs);
-$ll .= sprintf(".\n     Yours %s,\n     M.U.C.\n\n", $adv);
+$ll .= sprintf(".\n Yours %s,\n J.R.C.\n\n", $adv);
 
 echo "<em>";
-echo str_replace('  ', ' ', $ll);
+echo str_replace(' ',' ', $ll);
 echo "</em>";
 
 echo "<ul>";
@@ -125,23 +126,28 @@ echo "</ul>";
 
 //returns random value from array
 function rel($arr) {
-	return $arr[array_rand($arr)];
+return $arr[array_rand($arr)];
 }
 
 function print_ra($arr) {
-	foreach ($arr as $el) {
-		echo "$el\n";
-	}
+foreach ($arr as $el) {
+echo "$el\n";
+}
 }
 
 ?>
 <br clear="both">
 <strong>REFERENCES</strong>
+<a href="http://www.gingerbeardman.com/loveletter/">http://www.gingerbeardman.com/loveletter/</a>
+<a href="https://github.com/gingerbeardman/loveletter">https://github.com/gingerbeardman/loveletter</a>
 <a href="http://www.mosi.org.uk/about-us/news/mosi-writes-love-letter-to-ferranti-(1).aspx">http://www.mosi.org.uk/about-us/news/mosi-writes-love-letter-to-ferranti-(1).aspx</a>
 <a href="http://grandtextauto.org/2005/08/01/christopher-strachey-first-digital-artist/">http://grandtextauto.org/2005/08/01/christopher-strachey-first-digital-artist/</a>
+<a href="http://64.62.200.70/PERIODICAL/PDF/Encounter-1954oct/27-36/">http://64.62.200.70/PERIODICAL/PDF/Encounter-1954oct/27-36/</a>
 <a href="http://gnoetrydaily.wordpress.com/2010/07/13/2-strachey-love-letters/">http://gnoetrydaily.wordpress.com/2010/07/13/2-strachey-love-letters/</a>
-<a href="http://www.alpha60.de/art/love_letters/">http://www.alpha60.de/art/love_letters/</a>
-
+<a href="http://alpha60.de/research/muc/">http://www.alpha60.de/art/love_letters/</a>
+<a href="http://www.alpha60.de/research/there_must_be_an_angel/DavidLink_MustBeAnAngel_2006.pdf">http://www.alpha60.de/research/there_must_be_an_angel/DavidLink_MustBeAnAngel_2006.pdf</a>
+<a href="http://www.samplereality.com/2013/01/08/an-account-of-randomness-in-literary-computing/">http://www.samplereality.com/2013/01/08/an-account-of-randomness-in-literary-computing/</a>
 </pre>
 </body>
 </html>
+
